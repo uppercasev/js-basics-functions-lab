a912c28a7a6fa9e6f92f048871ca094d0860677f
Vi Tran <contact.vitran@gmail.com>
2021-10-16 21:54:36 +1100

    Finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(streetBlock) {
+    if (streetBlock >= 42) {
+        return (streetBlock - 42);
+    } else {
+        return (42- streetBlock);
+    }
+}
+
+function distanceFromHqInFeet(streetBlock) {
+    return distanceFromHqInBlocks(streetBlock) * 264;
+}
+
+function distanceTravelledInFeet(pickUpBlock, destinationBlock) {
+    if (pickUpBlock > destinationBlock) {
+        return (pickUpBlock - destinationBlock) * 264;
+    } else {
+        return (destinationBlock - pickUpBlock) *264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance <= 2500) {
+        return 25; 
+    } else {
+        return 'cannot travel that far';
+    }
+}
